e1639e7350138f1cad78749c9fe6cbcc4e1ed9ad
Taran Lynn <taranlynn0@gmail.com>
2019-06-28 00:00:42 -0700

    Remove unnecessary pi and move t to be first parameter.

--- src/HiddenMarkov.py
+++ src/HiddenMarkov.py
@@ -10,15 +10,18 @@ class HiddenMarkov:
 
         self.transitions = np.random.random((nodes, nodes))
         self.emissions   = np.random.random((nodes, outputs))
-        self.pi          = np.random.random(nodes)
 
-        self.transitions /= self.transitions.sum()
-        self.emissions   /= self.emissions.sum()
-        self.pi          /= self.pi.sum()
+        #self.transitions[:,:] = 1/nodes
+        #self.emissions[:,:] = 1/outputs
 
         self.randGen = random.Random()
         self.state = 0
 
+
+    # TODO
+    # 1. Change to handle multiple samples.
+    # 2. Optimize using matrix operations. Haven't done this yet because it's
+    #    tricky
     def train(self, samples, iterations):
         """
         Baum–Welch algorithm
@@ -29,23 +32,23 @@ class HiddenMarkov:
             a = self.forward(s)
             b = self.backward(s)
 
-            y = np.zeros((self.nodes, len(s)))
-            e = np.zeros((self.nodes, self.nodes, len(s) - 1))
+            y = np.zeros((len(s), self.nodes))
+            e = np.zeros((len(s) - 1, self.nodes, self.nodes))
 
             for t in range(len(s)):
                 for i in range(self.nodes):
-                    y[i, t] = a[i, t]*b[i, t]
+                    y[t, i] = a[t, i]*b[t, i]
 
-                if y[:, t].sum() > 0:
-                    y[:, t] /= y[:, t].sum()
+                if y[t, :].sum() > 0:
+                    y[t, :] /= y[t, :].sum()
 
             for t in range(len(s) - 1):
                 for i in range(self.nodes):
                     for j in range(self.nodes):
-                        e[i,j,t] = a[i, t]*self.transitions[i,j]*b[j, t+1]*self.emissions[j, s[t + 1]]
+                        e[t,i,j] = a[t, i]*self.transitions[i,j]*b[t+1, j]*self.emissions[j, s[t + 1]]
 
-                if e[:, :, t].sum() > 0:
-                    e[:,:,t] /= e[:,:,t].sum()
+                if e[t,:,:].sum() > 0:
+                    e[t,:,:] /= e[t,:,:].sum()
 
             # Update transitions
             for i in range(self.nodes):
@@ -54,8 +57,8 @@ class HiddenMarkov:
                     ysum = 0
 
                     for t in range(len(s) - 1):
-                        esum += e[i, j, t]
-                        ysum += y[i, t]
+                        esum += e[t, i, j]
+                        ysum += y[t, i]
 
                     if ysum > 0:
                         self.transitions[i,j] = esum/ysum
@@ -68,43 +71,48 @@ class HiddenMarkov:
 
                     for t in range(len(s)):
                         if s[t] == v:
-                            ysumCond += y[i, t]
-                        ysum += y[i, t]
+                            ysumCond += y[t, i]
+                        ysum += y[t, i]
 
                     if ysum > 0:
                         self.emissions[i, v] = ysumCond/ysum
 
-            # Update Pi
-            self.pi = y[:,0].copy()
+            # Add some randomness so that we don't overfit.
+            self.transitions += np.random.random(self.transitions.shape)/100
+            self.transitions /= self.transitions.sum()
+
+            self.emissions += np.random.random(self.emissions.shape)/100
+            self.emissions /= self.emissions.sum()
 
     def forward(self, sample):
-        a = np.zeros((self.nodes, len(sample)))
+        a = np.zeros((len(sample), self.nodes))
 
         for i in range(self.nodes):
-            a[i,0] = self.pi[i] * self.emissions[i, sample[0]]
+            a[0,i] = self.emissions[i, sample[0]]
 
         for t in range(1, len(sample)):
             for i in range(self.nodes):
-                a[i, t] = self.emissions[i, sample[t]] * sum([a[j, t - 1] * self.transitions[i, j] for j in range(self.nodes)])
+                a[t, i] = self.emissions[i, sample[t]] * sum([a[t - 1, j] * self.transitions[i, j] for j in range(self.nodes)])
 
         return a
 
     def backward(self, sample):
-        b = np.zeros((self.nodes, len(sample)))
+        b = np.zeros((len(sample), self.nodes))
 
         for i in range(self.nodes):
-            b[i, -1] = self.pi[i] * self.emissions[i, sample[-1]]
+            b[-1, i] = self.emissions[i, sample[-1]]
 
         for tpre in range(1, len(sample)):
             t = len(sample) - 1 - tpre
 
             for i in range(self.nodes):
-                b[i, t] = 0
+                b[t, i] = 0
 
                 for j in range(self.nodes):
-                    b[i, t] += b[j, t+1] * self.transitions[i,j] * self.transitions[j, sample[t + 1]]
+                    b[t, i] += b[t+1, j] * self.transitions[i,j] * self.transitions[j, sample[t + 1]]
 
         return b
+
 
     def generate(self, length, seed=None):
         if seed is not None:
@@ -124,3 +132,12 @@ class HiddenMarkov:
 
     def transNext(self):
         self.state = self.randGen.choices(range(self.nodes), self.transitions[self.state,:])[0]
+
+
+if __name__ == '__main__':
+    hmm = HiddenMarkov(100, 26)
+    sample = list(map(lambda c: ord(c) - ord('a'), "bumblebee"))
+    hmm.train([sample], 10)
+    print(hmm.transitions)
+    print(hmm.emissions)
+    print(''.join(map(lambda n: chr(n + ord('a')), hmm.generate(30))))
